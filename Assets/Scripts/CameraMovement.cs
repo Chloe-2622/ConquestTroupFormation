fe397f7c127c6f4e415e19b6164f8cf90b3690ae
ThomasSchneider-94 <traschneider@gmail.com>
2024-01-01 14:31:17 +0100

    NIS Camera Movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class CameraMovement : MonoBehaviour
 {
     [Header("Movement Customisation")]
     [SerializeField] private float defaultSpeed = 10f;
+    [SerializeField] private float sprintMultiplier = 2f;
     //[SerializeField] private float borderSize = 50f;
 
     [Header("Input System")]
     [SerializeField] private InputActionReference movement;
     [SerializeField] private InputActionReference sprint;
+    [SerializeField] private InputActionReference zoom;
 
     private Camera cameraObject;
     private bool isPaused = false;
@@ -23,12 +28,62 @@ public class CameraMovement : MonoBehaviour
         cameraObject = GetComponent<Camera>();
     }
 
+    private void OnEnable()
+    {
+        movement.action.Enable(); // Activer l'action d'entrée lorsque le script est activé
+        sprint.action.Enable();
+        zoom.action.Enable();
+
+        zoom.action.started += zoomCamera;
+    }
+
+    // On se désabonne aux évènements du Event System
+    private void OnDisable()
+    {
+        movement.action.Disable(); // Désactiver l'action d'entrée lorsque le script est désactivé
+        sprint.action.Disable();
+        zoom.action.Disable();
+
+        zoom.action.started -= zoomCamera;
+    }
+
+
+    public void zoomCamera(InputAction.CallbackContext context)
+    {
+        Vector2 zoomVector = zoom.action.ReadValue<Vector2>();
+        zoomVector.Normalize();
+
+        Debug.Log(zoomVector);
+
+        // Adjust the speed of scrolling by multiplying with a factor if needed
+        float scrollSpeed = transform.position.y / 10;
+
+        // Calculate the new position
+        Vector3 newPosition = transform.position + (transform.forward * zoomVector.y * scrollSpeed);
+        Debug.Log(newPosition);
+
+        // Clamp the y component between 6 and 30
+        newPosition.y = Mathf.Clamp(newPosition.y, 6f, 30f);
+
+        // Apply the new position
+        if ((newPosition.y == 6f && zoomVector.y > 0) || (newPosition.y == 30f && zoomVector.y < 0))
+        {
+            // Scroll is not allowed at the current position, do not update the position
+        }
+        else
+        {
+            // Apply the new position
+            transform.position = newPosition;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
 
         float currentSpeed;
-        if (sprint.action.inProgress) { currentSpeed = 2 * defaultSpeed;}
+        if (sprint.action.inProgress) {
+            currentSpeed = sprintMultiplier * defaultSpeed;}
         else { currentSpeed = defaultSpeed; }
 
         float dx = 0f;
@@ -36,48 +91,25 @@ public class CameraMovement : MonoBehaviour
         
         Vector2 movementInput = movement.action.ReadValue<Vector2>();
 
-        if (movementInput.x > 0 || Input.GetKey(KeyCode.D))
+        if (movementInput.x > 0)
         {
             dx++;
             dz--;
         }
-        if (movementInput.x < 0 || Input.GetKey(KeyCode.A))
+        if (movementInput.x < 0)
         {
             dx--;
             dz++;
         }
-        if (movementInput.y > 0 || Input.GetKey(KeyCode.W))
+        if (movementInput.y > 0)
         {
             dx++;
             dz++;
         }
-        if (movementInput.y < 0 || Input.GetKey(KeyCode.S))
+        if (movementInput.y < 0)
         {
             dx--;
             dz--;
-        }
-
-        if (Input.mouseScrollDelta.y != 0)
-        {
-            // Adjust the speed of scrolling by multiplying with a factor if needed
-            float scrollSpeed = transform.position.y / 10;
-
-            // Calculate the new position
-            Vector3 newPosition = transform.position + (transform.forward * Input.mouseScrollDelta.y * scrollSpeed);
-
-            // Clamp the y component between 6 and 30
-            newPosition.y = Mathf.Clamp(newPosition.y, 6f, 30f);
-
-            // Apply the new position
-            if ((newPosition.y == 6f && Input.mouseScrollDelta.y > 0) || (newPosition.y == 30f && Input.mouseScrollDelta.y < 0))
-            {
-                // Scroll is not allowed at the current position, do not update the position
-            }
-            else
-            {
-                // Apply the new position
-                transform.position = newPosition;
-            }
         }
 
         if (!isPaused)
